127806660163698bb1c698162ecf70caa5eb59a2
faragelo <khaledfarghaly011@gmail.com>
2024-09-28 18:58:07 +0300

    Added configuration file for API keys and service credentials

---
+++ config/config.py
@@ -0,0 +1,20 @@
+config = {
+    "openai": {
+        "api_key": "Your API Key"
+    },
+    "gemini": {
+        "api_key": "Your API Key"
+    },
+    "kafka": {
+        "sasl.username": "Your Username",
+        "sasl.password": "Your Password",
+        "bootstrap.servers": "Your Server",
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'session.timeout.ms': 50000
+    },
+    "elastic_search": {
+        "url": "Your API url",
+        "api_key": "Your API Key"
+    }
+}
